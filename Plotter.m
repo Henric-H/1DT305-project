aaa0c51d9d541b983589e8dc2f2a5737d8c357d1
Henric-H <66855391+Henric-H@users.noreply.github.com>
2020-07-02 02:59:02 +0200

    Create Plotter.m

---
+++ Plotter.m
@@ -0,0 +1,28 @@
+% Enter your MATLAB code below
+readChId = 1080128              % Your Channel ID					
+readKey = 'H7FKV5N7VR28IWPH' %Your Read API Key
+[Data1, Time1] = thingSpeakRead(readChId,'fields',[1,4,3,2,5],...
+    'NumPoints',500,'ReadKey',readKey);
+
+[Data2, Time2] = thingSpeakRead(readChId,'fields',[6],...
+    'NumPoints',500,'ReadKey',readKey);
+
+
+%% title('Temperature and Humidity'); %%
+
+
+%% Temperature and Humidity Plots %%
+%%yyaxis left;%%
+plot(Time1, Data1);
+ylabel('Temprature and Humidity');
+xlabel('Date & Time (GMT+0)');
+
+%% Pressure Plots %%
+yyaxis right;
+plot(Time2, Data2);
+ylabel('Pressure');
+
+legend({'Temp DDT11 (C)','Temp BME280 (C)','Temp ESP32 (C)','Hum DDT11 (%)','Hum BME280 (%)','Pres BME280 (hPa)'});
+legend('Location','best')
+
+grid on;
